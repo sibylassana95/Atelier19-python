86675bb148c5b9dfb998e1c807dac91317343980
Lassana siby <103085452+sibylassana95@users.noreply.github.com>
2022-06-21 20:30:24 +0000

    Update and rename atelier19.py to main.py

--- atelier19.py
+++
@@ -1,9 +0,0 @@
-# Écrire un programme, qui trace un hexagone (polygone à 6 côtés, angles interne à 120°)
-import turtle
-from turtle import *
-c=2
-u=100
-for k in range(6):
-    forward(c*u)
-    left(72)
-exitonclick()
---
+++ main.py
@@ -0,0 +1,8 @@
+import turtle
+from turtle import *
+c=2
+u=100
+for k in range(6):
+    forward(c*u)
+    left(72)
+exitonclick()
